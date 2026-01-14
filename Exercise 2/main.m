11187cd9fc3d28d1fb8454671ae040372627b461
Mohammad Karbalaee Shabani <muhammad.ksht@gmail.com>
2026-01-14 10:33:09 +0100

    Update main.m

--- Exercise 2/main.m
+++ Exercise 2/main.m
@@ -1,25 +1,24 @@
-load('iEMG_contraction.mat'); 
+load('iEMG_contraction.mat');
 
-numMUs = size(MUPulses, 1);
 numSamples = length(force_signal);
-time_vec = (0:numSamples-1) / fsamp;
-
-MUPulses_Seconds = cell(numMUs, 1);
-for i = 1:numMUs
-    MUPulses_Seconds{i} = MUPulses{i} / fsamp;
-end
+time = (0:numSamples-1) / fsamp;
 
 figure;
-
-yyaxis left;
-plotSpikeRaster(MUPulses_Seconds, 'PlotType', 'vertline', 'VertSpikeHeight', 0.9);
-ylabel('Motor Unit Number');
-ylim([0.5 numMUs + 0.5]);
-
-yyaxis right;
-plot(time_vec, force_signal);
-ylabel('Force (N)');
-
+plot(time, force_signal);
+title('Task 1.1: Force Signal over Time');
 xlabel('Time (s)');
-title('Task 1.1: MU Spike Trains and Force Signal');
+ylabel('Force (N)');
 grid on;
+
+% Convert sample indices to seconds for each motor unit
+MUPulses_sec = cell(size(MUPulses));
+for i = 1:length(MUPulses)
+    MUPulses_sec{i} = (double(MUPulses{i}) - 1) / fsamp;
+end
+
+% Plot using the cell array of times
+figure;
+plotSpikeRaster(MUPulses_sec, 'PlotType', 'vertline', 'VertSpikeHeight', 0.9);
+title('Task 1.1: Motor Unit Spike Trains');
+xlabel('Time (s)');
+ylabel('Motor Unit Number');
