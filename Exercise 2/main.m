0b5912a41e22cf1f0c2671d1f3969bab802cdaee
Mohammad Karbalaee Shabani <muhammad.ksht@gmail.com>
2026-01-14 10:33:43 +0100

    Update main.m

--- Exercise 2/main.m
+++ Exercise 2/main.m
@@ -3,22 +3,21 @@ load('iEMG_contraction.mat');
 numSamples = length(force_signal);
 time = (0:numSamples-1) / fsamp;
 
-figure;
-plot(time, force_signal);
-title('Task 1.1: Force Signal over Time');
-xlabel('Time (s)');
-ylabel('Force (N)');
-grid on;
-
-% Convert sample indices to seconds for each motor unit
 MUPulses_sec = cell(size(MUPulses));
 for i = 1:length(MUPulses)
     MUPulses_sec{i} = (double(MUPulses{i}) - 1) / fsamp;
 end
 
-% Plot using the cell array of times
 figure;
+
+yyaxis left
 plotSpikeRaster(MUPulses_sec, 'PlotType', 'vertline', 'VertSpikeHeight', 0.9);
-title('Task 1.1: Motor Unit Spike Trains');
-xlabel('Time (s)');
 ylabel('Motor Unit Number');
+
+yyaxis right
+plot(time, force_signal);
+ylabel('Force (N)');
+
+title('Task 1.1: MU Spike Trains and Force Signal over Time');
+xlabel('Time (s)');
+grid on;
